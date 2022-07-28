191b7caafc2f7292af59588c3765f862830e4ce7
rollrat <rollrat@naver.com>
2022-07-28 10:31:59 +0900

    Update neper.run.sh

--- neper.run.sh
+++ neper.run.sh
@@ -1,6 +1,6 @@
 for i in {1..20}
 do
-  ssh usernet-vm3 ./neper/tcp_stream > $i.txt &
+  ssh usernet-vm3 ./neper/tcp_stream > result/$i.txt &
   sleep 1
   ssh usernet-vm4 ./neper/tcp_stream -c -H 172.16.1.101 -l 5 > /dev/null
   echo $i
