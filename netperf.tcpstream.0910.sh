059e7253cc40dae9732d71d45d4369ce162fc423
rollrat <rollrat@naver.com>
2022-07-31 15:39:57 +0900

    Update netperf.tcpstream.0910.sh

--- netperf.tcpstream.0910.sh
+++ netperf.tcpstream.0910.sh
@@ -42,7 +42,7 @@ sleep 1
 
 # start netperf
 echo 2. start netperf
-ssh usernet-vm4 "netperf -H 172.16.1.103 -p 8864 -D 10 -l 200 -P 0 -- -o min_latency,mean_latency,max_latency,stddev_latency,transaction_rate > netperf.result.txt" &
+ssh usernet-vm4 "netperf -H 172.16.1.103 -p 8864 -D 10 -l 200 -P 0 > netperf.result.txt" &
 
 # sleep 40
 echo 3. sleep 90s
