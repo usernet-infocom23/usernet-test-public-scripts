73a939198ed3592f5169fc4c1f3b539d16b8dfaf
rollrat <rollrat@naver.com>
2022-07-31 18:23:02 +0900

    Update neper.run.sh

--- neper.run.sh
+++ neper.run.sh
@@ -2,8 +2,8 @@ ssh usernet-vm3 rm tcp_stream*
 for i in {1..40}
 do
   echo start $i
-  ssh usernet-vm3 ./neper/tcp_stream > result.neper/$i.txt &
+  ssh usernet-vm3 sudo LIBUSERNET_IVSHMEM_MEMDEV_PATH="/sys/bus/pci/devices/0000:07:00.0/resource2_wc" LD_PRELOAD="./libusernet.dummy.so" ./neper/tcp_stream > result.neper/$i.txt &
   sleep 1
-  ssh usernet-vm4 ./neper/tcp_stream -c -H 172.16.1.103 -l 8 -F 8 -T 4 > /dev/null
+  ssh usernet-vm4 sudo LIBUSERNET_IVSHMEM_MEMDEV_PATH="/sys/bus/pci/devices/0000:07:00.0/resource2_wc" LD_PRELOAD="./libusernet.dummy.so" ./neper/tcp_stream -c -H 172.16.1.103 -l 8 -F 8 -T 4 > /dev/null
   echo end $i
 done
