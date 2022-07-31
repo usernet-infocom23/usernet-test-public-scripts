476cd0c106329280f39874559816ae5e92d95ade
rollrat <rollrat@naver.com>
2022-07-31 18:33:51 +0900

    Update neper.tcprr.run.sh

--- neper.tcprr.run.sh
+++ neper.tcprr.run.sh
@@ -2,8 +2,8 @@ ssh usernet-vm3 rm tcp_stream*
 for i in {1..40}
 do
   echo start $i
-  ssh usernet-vm3 ./neper/tcp_rr > result.neper/$i.txt &
+  ssh usernet-vm3 sudo LIBUSERNET_IVSHMEM_MEMDEV_PATH="/sys/bus/pci/devices/0000:07:00.0/resource2_wc" LD_PRELOAD="./libusernet.dummy.so" ./neper/tcp_rr > result.neper/$i.txt &
   sleep 1
-  ssh usernet-vm4 ./neper/tcp_rr -c -H 172.16.1.103 -l 8 > /dev/null
+  ssh usernet-vm4 sudo LIBUSERNET_IVSHMEM_MEMDEV_PATH="/sys/bus/pci/devices/0000:07:00.0/resource2_wc" LD_PRELOAD="./libusernet.dummy.so" ./neper/tcp_rr -c -H 172.16.1.103 -l 8 > /dev/null
   echo end $i
 done
