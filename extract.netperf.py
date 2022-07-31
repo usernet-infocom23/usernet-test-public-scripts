9b1231fdc6044b4e41e685ef4c2f9c226b19c8c8
rollrat <rollrat@naver.com>
2022-07-31 15:56:25 +0900

    Update extract.netperf.py

--- extract.netperf.py
+++ extract.netperf.py
@@ -7,8 +7,7 @@ if len(sys.argv) < 2:
 with open(sys.argv[1], 'r', encoding='utf-8') as f:
     lc = 1
     for line in f.readlines():
-        if line.startswith('Interim result:'):
-            line = line[len('Interim result:'):]
-            bitsps = line.strip().split(' ')[0]
+        if "bits/s" in line:
+            bitsps = line.strip().split(',')[0]
             print('{},{}'.format(lc, bitsps))
             lc += 1
